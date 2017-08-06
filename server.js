d3517e54e65ade81c105f2bb780a372e3c3f430d
Cathy Retina 13 <a304126@gmail.com>
2017-08-06 14:55:32 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+import webpack from 'webpack';
+import WebpackDevServer from 'webpack-dev-server';
+import config from './webpack.config.dev.babel';
+import ip from 'ip';
+import websiteJson from './config/website.json';
+
+new WebpackDevServer(webpack(config), {
+	contentBase: config.output.path,
+	publicPath: config.output.publicPath,
+	hot: true,
+	noInfo: true,
+	overlay: true,
+}).listen(websiteJson.port.webpackServer, 'localhost', (err, result) => {
+	if (err) {
+		console.log(result);
+		return;
+	}
+
+	return;
+});
---
+++ webpack.config.build.babel.js
@@ -0,0 +1,71 @@
+import path from 'path';
+import webpack from 'webpack';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+import base from './webpack.config.babel';
+import websiteJson from './config/website.json';
+
+const webpackBuildConfig = {
+	entry: (() => {
+		for (const key in base.entry) {
+			if ({}.hasOwnProperty.call(base.entry, key)) {
+				Object.assign(base.entry, {
+					[key]: ['babel-polyfill', base.entry[key]],
+				});
+			}
+		}
+		return base.entry;
+	})(),
+	output: {
+		path: path.join(__dirname, 'build', websiteJson.multiLanguage ? 'assets' : ''),
+		filename: '[name]__[hash:13].chunk.js',
+		publicPath: websiteJson.multiLanguage ?
+			(websiteJson.domain.asset.value ? `//${websiteJson.domain.asset.value}/` : '/assets/')
+		:
+			'/',
+	},
+	module: {
+		rules: base.module.rules.concat(
+			Object.assign(base.module.cssRule, {
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: `${base.module.cssRule.use.css}?minimize=true!${base.module.cssRule.use.postcss}`,
+				}),
+			})
+		),
+	},
+	plugins: [
+
+		base.plugins.webpackEnvironment,
+
+		base.plugins.copyWebpackPlugin,
+
+		base.plugins.providePlugin,
+
+		base.plugins.loaderOptionsPlugin,
+
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'bundle',
+			filename: 'bundle__[hash:13].min.js',
+		}),
+
+		new webpack.optimize.UglifyJsPlugin({
+			sourceMap: true,
+		}),
+
+		new CopyWebpackPlugin([
+			{
+				from: 'src/robots.txt',
+				to: 'robots.txt',
+			},
+		]),
+
+		// Extract the CSS into a seperate file
+		new ExtractTextPlugin('[name]__[contenthash:13].min.css'),
+
+	].concat(base.plugins.htmlWebpackPlugin),
+	devtool: 'source-map',
+	externals: base.externals,
+};
+
+export default webpackBuildConfig;
