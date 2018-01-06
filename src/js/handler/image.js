33a5aead4b5bd9c0025ceadec5e68b9940a16277
Cathy Retina 13 <a304126@gmail.com>
2018-01-06 22:25:19 +0800

    feat: support user fill image url

--- src/js/handler/image.js
+++ src/js/handler/image.js
@@ -3,7 +3,10 @@ import variable from '../appConfig';
 
 const imageHandler = () => {
 	const range = variable.editor.getSelection();
-	const imageUrl = 'https://www.surveycake.com/common/images/mail/logo.png';
+
+	/* eslint-disable no-alert */
+	const imageUrl = window.prompt('請填入圖片網址');
+	/* eslint-enable no-alert */
 
 	variable.editor.insertEmbed(range.index, 'image', imageUrl, Quill.sources.USER);
 };
