16cdceec4da0e430ae455684e57db07ae930dd93
Cathy Retina 13 <a304126@gmail.com>
2018-01-24 19:43:58 +0800

    add comment for change block/embed to embed reason

--- src/js/register/answerTagBlot.js
+++ src/js/register/answerTagBlot.js
@@ -2,10 +2,11 @@ import Quill from 'quill';
 
 /**
  * `blots/inline` cannot support span tag
- * should using `blots/block/embed` instead
+ * should using `blots/block/embed` or `blots/embed` instead
  *
  * Reference:
  * https://stackoverflow.com/questions/37796409/is-it-possible-to-create-a-custom-format-blot-with-complex-substructure
+ * https://github.com/quilljs/quill/issues/853
  */
 
 const BlockEmbed = Quill.import('blots/embed');
